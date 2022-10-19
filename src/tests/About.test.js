c5bcaa85bc7fc36342f0a69bd491ff330d59f9b0
petromamboubee <gm.boubee@gmail.com>
2022-10-19 16:23:19 -0300

    terminei o req 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,7 @@
+// import React from 'react';
+// // import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// // import App from '../App';
+// import renderWithRouter from './renderWithRouter';
+
 test('', () => {});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+// import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../pages';
+
+test('testa se existe os links', () => {
+  const { history, getByText } = renderWithRouter(<App />);
+  const linkNav = (texto) => getByText(texto);
+  const { location: { pathname } } = history;
+  expect(linkNav('Home')).toBeInTheDocument();
+  expect(linkNav('About')).toBeInTheDocument();
+  expect(linkNav('Favorite Pokémons')).toBeInTheDocument();
+  expect(pathname).toBe('/');
+});
+
+test('testa se ao clicar no link Home volta para a pagina principal', () => {
+  const { history, getByText } = renderWithRouter(<App />);
+  const linkHome = getByText('Home');
+  userEvent.click(linkHome);
+  expect(history.location.pathname).toBe('/');
+});
+
+test('testa se ao clicar no link "About" volta para a pagina sobre', () => {
+  const { history, getByText } = renderWithRouter(<App />);
+  const linkAbout = getByText('About');
+  userEvent.click(linkAbout);
+  expect(history.location.pathname).toBe('/about');
+});
+
+test('testa se ao clicar no link "Favorite Pokémons" volta para a pagina favorites', () => {
+  const { history, getByText } = renderWithRouter(<App />);
+  const linkPK = getByText('Favorite Pokémons');
+  userEvent.click(linkPK);
+  expect(history.location.pathname).toBe('/favorites');
+});
+
+test('testa se ao entrar em uma url diferente ele cai em not found', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const pageNotFound = getByText('Page requested not found');
+  expect(pageNotFound).toBeInTheDocument();
+});
