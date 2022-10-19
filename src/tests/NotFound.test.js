eb51ac47a0f6af3d49e1b98b03190d4e1ca60b8f
petromamboubee <gm.boubee@gmail.com>
2022-10-19 17:26:53 -0300

    terminei o req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const titleText = getByRole('heading', { name: 'Page requested not found', level: 2 });
+  expect(titleText).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const img = getByAltText(/Pikachu crying because the page requested was not found/i);
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
