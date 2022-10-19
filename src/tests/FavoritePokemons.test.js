c1847a381c86c4dfa92e51e453d0e080f6599e92
petromamboubee <gm.boubee@gmail.com>
2022-10-19 17:14:56 -0300

    terminei o req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const unfoundFav = getByText(/no favorite pokemon found/i);
+  expect(unfoundFav).toBeInTheDocument();
+});
+
+// test('', () =>)
